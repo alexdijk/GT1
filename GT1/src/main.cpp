3e6152ddee3f30dac4319174b7e8d69dd6a52b90
alexdijk <alexdijk@outlook.com>
2019-12-15 20:37:47 +0100

    integrated sfml and imgui as a test, based on merge.py

--- GT1/src/main.cpp
+++ GT1/src/main.cpp
@@ -1,14 +1,53 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 #include "state.h"
+#include "single_imgui.hpp"
 
 int main(int argc, char** argv)
 {
 	State s;
 
-	std::cout << "Text A: " << s.getText() << std::endl;
-	
-	s.setText(std::string("Cool B"));
+	sf::RenderWindow win(sf::VideoMode(800, 600), "Main window");
+	ImGui::SFML::Init(win);
 
-	std::cout << "Text B: " << s.getText() << std::endl;
+	sf::Clock timeDelta;
+
+	bool showframerate{ false };
+
+	while (win.isOpen())
+	{
+		sf::Event ev;
+		while (win.pollEvent(ev))
+		{
+			ImGui::SFML::ProcessEvent(ev);
+
+			switch (ev.type)
+			{
+			case sf::Event::Closed:
+				win.close();
+				break;
+			default:
+				break;
+			}
+		}
+
+		ImGui::SFML::Update(win, timeDelta.restart());
+
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::Checkbox("frame rate", &showframerate);
+		if (showframerate)
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+
+		win.clear();
+		// win.draw();
+		ImGui::SFML::Render(win);
+		win.display();
+	}
+
+	ImGui::SFML::Shutdown();
 
 	return EXIT_SUCCESS;
 }
